65599c045f5b72bc655742a8b100ff834fb9d572
tomzumbi <114890330+tomzumbi@users.noreply.github.com>
2022-11-02 10:30:54 +0700

    Add files via upload

---
+++ ktraSo Nguyen To/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("kiem tra so nguyen to");
+        int a = scanner.nextInt();
+        int dem = 0;
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0){
+                dem ++;
+            }
+        }
+        if (dem == 2){
+            System.out.println( "la so nguyen to");
+        }else {
+            System.out.println("khong phai");
+        }
+    }
+}
